b9be719a678da55a7388862b5738ed8a0b8534b1
ehsndvr <ehsndvr81@gmail.com>
2026-02-18 00:32:31 +0330

    fix colors

--- ezssl/ehsndvr-ezssl.sh
+++ ezssl/ehsndvr-ezssl.sh
@@ -12,13 +12,13 @@ NON_INTERACTIVE="false"
 USE_MENU="false"
 TTY_FD=0
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-CYAN='\033[0;36m'
-BOLD='\033[1m'
-NC='\033[0m'
+RED=$'\033[0;31m'
+GREEN=$'\033[0;32m'
+YELLOW=$'\033[1;33m'
+BLUE=$'\033[0;34m'
+CYAN=$'\033[0;36m'
+BOLD=$'\033[1m'
+NC=$'\033[0m'
 
 log() {
   echo -e "[${TAG}] $*"
